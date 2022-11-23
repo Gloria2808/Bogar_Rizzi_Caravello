07be26e5f800e5df4d07750edeb037326dcd19de
Eugenio Amato Nadaia <eugenio.nadaia@isitgo.it>
2022-11-23 09:00:33 +0100

    Commento

--- JavaApplication3/src/bogarrizzicaravello/Main.java
+++ JavaApplication3/src/bogarrizzicaravello/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package bogarrizzicaravello;
+package bogarrizzicaravello; //Package
 
 /**
  *
  * @author 4ib
  */
-public class Main {
+public class Main { 
 
     /**
      * @param args the command line arguments
